b5e5b796f8dd83174617cf7ec34d6ea6b4179677
ingrid fleury <ingridfleury1@gmail.com>
2021-08-13 01:13:16 +0100

    creation of main components,  project pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { NewRecipeComponent } from './new-recipe/new-recipe.component';
+import { RegisterComponent } from './register/register.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RecipeComponent,
+    NewRecipeComponent,
+    RegisterComponent,
+    MyProfileComponent
   ],
   imports: [
     BrowserModule,
