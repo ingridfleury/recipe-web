26118f38cb362c8f1acc770f1a837aacb215b7fd
ingrid fleury <ingridfleury1@gmail.com>
2021-08-19 23:57:18 +0100

    Routing implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { LoginComponent } from './login/login.component';
+import { NewRecipeComponent } from './new-recipe/new-recipe.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: 'new-recipe', component: NewRecipeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'recipe', component: RecipeComponent },
+  { path: 'my-profile', component: MyProfileComponent }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
